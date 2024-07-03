a23df06697c63ab95f294e48b99439ad914cd4a8
olegoleg0080 <120270714+olegoleg0080@users.noreply.github.com>
2024-07-03 09:55:53 +0300

    sddsfds

--- src/pages/ToDo.jsx
+++ src/pages/ToDo.jsx
@@ -92,6 +92,7 @@ export const ToDo = () => {
             )}
             {isShowModal && <ConfirmModal delToDo={delToDo} />}
             {isShowRedactModal && <RedactModal redactToDo={redToDo} />}
+            
         </>
     );
 };
