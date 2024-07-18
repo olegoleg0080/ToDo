29169085f288feb786c419b8b93115e60cb2f1bb
olegoleg0080 <120270714+olegoleg0080@users.noreply.github.com>
2024-07-18 14:48:42 +0300

    fsdfsdfsdf

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     // <React.StrictMode>
     <ThemeProvider theme={theme}>
-        <BrowserRouter basename="/ToDo">
+        <BrowserRouter basename="ToDo">
             <Provider store={store}>
                 <PersistGate loading={null} persistor={persistor}>
                     <App />
